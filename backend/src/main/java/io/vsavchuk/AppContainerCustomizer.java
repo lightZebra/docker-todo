4ed3b53ef602cde0b4bc2302ec8ddd7c0cc696eb
Vladyslav Savchuk <logicvsua@gmail.com>
2020-02-23 23:31:36 +0200

    Improve pom.xml and use aws port

---
+++ backend/src/main/java/io/vsavchuk/AppContainerCustomizer.java
@@ -0,0 +1,14 @@
+package io.vsavchuk;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppContainerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+	@Override
+	public void customize(ConfigurableWebServerFactory factory) {
+		factory.setPort(Integer.parseInt(System.getenv().getOrDefault("PORT", "8081")));
+	}
+}
